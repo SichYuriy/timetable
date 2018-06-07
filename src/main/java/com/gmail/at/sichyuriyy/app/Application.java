96519638caf9e8d8cb346969616a1d1387c99a93
SichYuriy <sichyuriyy@gmail.com>
2018-06-07 14:38:33 +0300

    Set session timeout to the 24h

--- src/main/java/com/gmail/at/sichyuriyy/app/Application.java
+++ src/main/java/com/gmail/at/sichyuriyy/app/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan(basePackages = "com.gmail.at.sichyuriyy.*")
 @SpringBootApplication
 @Configuration
+@PropertySource("classpath:application.properties")
 public class Application {
 
     public static void main(String[] args) {
